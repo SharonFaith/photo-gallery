dae825ee320d6fa3e0702b5679b5a103d5af2545
SharonFaith <sharonfaith15@gmail.com>
2020-11-16 01:42:09 +0300

    add location and category models

--- photos/models.py
+++ photos/models.py
@@ -2,11 +2,28 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.category
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location
+
 class Image(models.Model):
     #image = models.ImageField(upload_to = 'articles/')
     image_name = models.CharField(max_length=60)
     image_description = models.TextField()
-#    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#    location = models.ForeignKey(Editor, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='other')
+    location = models.ForeignKey(Location, on_delete=models.RESTRICT, default='other')
     upload_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.image_name
