0d755e43113fc09648149b411335719887dc475e
SharonFaith <sharonfaith15@gmail.com>
2020-11-16 01:23:05 +0300

    create first image model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    #image = models.ImageField(upload_to = 'articles/')
+    image_name = models.CharField(max_length=60)
+    image_description = models.TextField()
+#    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#    location = models.ForeignKey(Editor, on_delete=models.RESTRICT)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
