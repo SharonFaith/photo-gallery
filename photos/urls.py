4343218d458a1b4ce9cf90a0183b625d2dd3e245
SharonFaith <sharonfaith15@gmail.com>
2020-11-16 08:30:27 +0300

    add tester modal

--- photos/urls.py
+++ photos/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     path('', welcome, name = 'welcome'),
     path('landing-page/', index, name = 'index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
